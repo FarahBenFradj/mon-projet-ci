5b39c81c0d160200b35772a0a24a741f7aa88c08
FarahBenFradj <farah.benfradj@itbs.tn>
2026-04-22 19:46:59 +0100

    feat: add monitoring config - Prometheus + Alerting

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,15 @@ const cors = require('cors');
 
 const app = express();
 
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
 app.use(cors());
 app.use(express.json());
 
